39c5eda2c6142b1d2b0664471cb6b2fab2821456
simone <sorzesimone1@gmail.com>
2021-04-08 21:57:50 +0200

    implementing submitclick

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { execute } from "./app";
+import { submitClick } from "./submit";
 import "../styles/sass/main.scss";
 
-execute();
+// execute();
+const form = document.getElementById("main-form");
+// Adding listener on Form submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitClick();
+});
---
+++ src/submit.js
@@ -0,0 +1,7 @@
+export const submitClick = () => {
+  let wattage = document.getElementById("wattage").value;
+  let usage = document.getElementById("usage").value;
+  let price = document.getElementById("price").value;
+
+  console.log(wattage, usage, price);
+};
